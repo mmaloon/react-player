14abfa8a04ced86a2d881119845c0eec0e985565
Mike Maloon <michael.maloon@gmail.com>
2019-08-13 14:43:42 -0700

    update terser plugin options

--- webpack.demo.babel.js
+++ webpack.demo.babel.js
@@ -34,12 +34,14 @@ export default {
   ],
   optimization: {
     minimizer: [
-    new TerserPlugin({
+      new TerserPlugin({
         cache: true,
         parallel: true,
         sourceMap: true, // Must be set to true if using source-maps in production
         terserOptions: {
-          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+          mangle: { // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+            reserved: ['ReactPlayer']
+          }
         }
       })
     ]
